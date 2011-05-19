fc6db8c8561dcb33da4a4872469ce2906c5e5ad9
Will Owen <will.s.owen@gmail.com>
2011-05-19 07:24:39 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestSpree::Application.config.secret_token = '1e0457e05fec9e6cf60098ad8e19dc81dda25b80c5430e8c1c984a619243e9174d1bde771d31d19bd5607d86dab12f8f83df6aca92fc2652df969f2cc4db339f'
