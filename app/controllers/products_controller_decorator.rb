88040f2e8a05d4810c3edb1dace44f0fa4ee8fce
Will Owen <will.s.owen@gmail.com>
2011-06-02 00:08:40 -0400

    music examples

--- app/controllers/products_controller_decorator.rb
+++ app/controllers/products_controller_decorator.rb
@@ -2,7 +2,7 @@ ProductsController.class_eval do
 public
     def rate
 		@product = Product.id_equals(params[:id])
-		@current_user = User.current
+		#@current_user = User.current
 		@rating = @product.ratings.mine
 
 		redirect_to "/products/#{params[:id]}"
